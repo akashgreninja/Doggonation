9fe4ba8041854297787c2846bb313e7cec138a49
akashgreninja <akashuhulekal@gmail.com>
2023-04-08 20:18:15 +0530

    added azure

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@
 from flask import Flask,jsonify,abort
 from dotenv import load_dotenv
 import os
+import pyodbc
 
 
 # all internal modules here
@@ -10,15 +11,28 @@ from  getrequests import Put
 
 
 
+
 app = Flask(__name__)
 load_dotenv()
 app_port = os.getenv('PORT')
+azure_password = os.getenv('PASSWORD')
+print(azure_password)
 put_requests=Put()
+
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:doggonation.database.windows.net,1433;Database=doggonation;Uid=doggonation;Pwd='+azure_password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+print(conn)
+
+
+
 
 
 @app.route('/')
 def route_path():
     return put_requests.startup()
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
