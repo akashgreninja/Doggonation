a4a123f92caad6b1d101528d542265ec4afca603
akashgreninja <akashuhulekal@gmail.com>
2023-04-12 12:45:06 +0530

    added the text analysis code

---
+++ backend/sentimentanalysis.py
@@ -0,0 +1,47 @@
+
+# some import notes these are the dependency modules you need to install before running this code
+# pip install transformers
+# pip install torch
+# pip install scipy
+
+
+
+
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
+
+class SentimentAnalysis:
+    
+
+    def __init__(self):
+        self.MODEL=f"cardiffnlp/twitter-roberta-base-sentiment"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.MODEL)
+
+    def get_sentiment(self,text):
+        encoded_text=self.tokenizer(text,return_tensors='pt')
+        output=self.model(**encoded_text)
+        scores=output[0][0].detach().numpy()
+        scores=softmax(scores)
+        scores
+        scores_dict={
+            'negative':scores[0],
+            'neutral':scores[1],
+            'positive':scores[2]
+
+        }
+
+        print(scores_dict)
+        maximunval=max(scores_dict, key=scores_dict.get)
+        if maximunval == 'positive':
+            return 'positive'
+        elif maximunval == 'negative' and scores_dict['negative'] > 0.6:
+            return 'negative'
+        else:
+            return 'neutral'
+
+
+
+
+
