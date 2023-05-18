125d3bc94b8997d90c7f98f9850ef695c1c2cf34
2deadmen <kartikhegde.2002@gmail.com>
2023-05-18 19:31:08 +0530

    handle share done

--- frontend/src/components/posts/Post.js
+++ frontend/src/components/posts/Post.js
@@ -2,13 +2,51 @@ import React, { useState, useEffect, useRef } from "react";
 import image from "../../images/logo-white.png";
 import { BsThreeDots } from "react-icons/bs";
 import Comment from "./Comment";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import { like_post } from "../../api/likepost";
+import io from "socket.io-client";
 import { remove_like_post } from "../../api/unlikepost";
 import "./Post.css";
+import { Getallfollowersforuser } from "../../api/getallfollowers";
+
+
 
 const Post = (props) => {
+  const socket = io("http://localhost:3003");
+  const user_id=55
+  const style = {
+    position: "absolute",
+    top: "30%",
+    left: "83%",
+    float:"right",
+    transform: "translate(-50%, -50%)",
+    width: 350,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 0.5,
+  };
+  const [msgsent, setmsgsent] = useState(false)
+  const [roomid, setroomid] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = async() => {
+    setOpen(true);
+    let data=await Getallfollowersforuser(user_id)
+    setfollowing(data.data)
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setmsgsent(false)
+  }
+
+
   let toggle = false;
   let likeRef = useRef(null);
+  const [following, setfollowing] = useState([])
   const [likerender, setlikerender] = useState(null);
   const [loadcomments, setloadcomments] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
@@ -40,6 +78,22 @@ const Post = (props) => {
       }
     }
   };
+
+  const handleshare=async(reciever_id,msg)=>{
+    socket.emit("connectuser", { sender_id: user_id, reciever_id: 2 });
+    await socket.on("connection", (message) => {
+      setroomid(message["data"])
+
+  })
+  await socket.emit("message", {
+    data:msg ,
+    room_id: roomid,
+    sender_id: user_id,
+    reciever_id: reciever_id,
+  });
+  setmsgsent(true)
+   
+}
   return (
     <div className="w-full bg-white  mb-2 mt-2 rounded-lg border-2 border-grey-500">
       <div className="flex flex-col w-full">
@@ -119,9 +173,40 @@ const Post = (props) => {
               )}
               Comment
             </button>
-            <button className="interaction-button">
+            <button onClick={handleOpen} className="interaction-button">
               <i class="fa-regular fa-2x fa-paper-plane"></i>Share
             </button>
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+               friends
+               {
+                following.map((ele)=>{
+                  return <div className="flex flex-row" key={ele}>
+                   <img
+              src={ele[6]}
+              alt="Profile Image"
+              className="rounded-full w-10 h-10 mb-3 mx-2"
+            />
+             <h3 className="mx-3">
+              {ele[3]}
+             </h3>
+             <div className="mx-2 w-2/12 "><button onClick={()=>handleshare(ele[0],element[0])} className="interaction-button border">
+             
+             {
+              msgsent?"sent":<i class="fa-regular fa-2x fa-paper-plane"></i>
+             }
+             </button></div>
+
+                  </div>
+                })
+               }
+            </Box>
+          </Modal>
           </div>
           <div
             ref={commentRef}
